38c0e2a2f42b21975cafad270e9b0a21d58f9b4b
icifra <99347099+icifra@users.noreply.github.com>
2023-03-04 14:35:13 +0000

    Update logo.js

--- js/logo.js
+++ js/logo.js
@@ -1,4 +1,15 @@
-window.addEventListener('load', () => {
-  const logo = document.querySelector('.h-logo img');
-  logo.classList.add('rotate');
+window.addEventListener('DOMContentLoaded', () => {
+  const logoImg = document.querySelector('.h-logo img');
+  if (logoImg.complete) {
+    logoImg.classList.add('rotate');
+  } else {
+    logoImg.addEventListener('load', () => {
+      logoImg.classList.add('rotate');
+    });
+  }
 });
+
+// window.addEventListener('load', () => {
+//   const logo = document.querySelector('.h-logo img');
+//   logo.classList.add('rotate');
+// });
