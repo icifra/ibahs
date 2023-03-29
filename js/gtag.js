c751265f5bc80397717233285a87dd618c305ff6
icifra <99347099+icifra@users.noreply.github.com>
2023-03-29 03:51:56 +0000

    Update gtag.js

--- js/gtag.js
+++ js/gtag.js
@@ -1,3 +1,15 @@
+// Функция для загрузки скрипта gtag
+function loadGtagScript() {
+  if (!window.gtagScriptLoaded) {
+    var script = document.createElement('script');
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-GDCEBH1ZZZ';
+    script.async = true;
+    document.head.appendChild(script);
+    window.gtagScriptLoaded = true;
+  }
+}
+loadGtagScript();
+
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
