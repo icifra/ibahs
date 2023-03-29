e90f8383b14ae818d18fcc65563f24239a4d710f
icifra <99347099+icifra@users.noreply.github.com>
2023-03-29 04:13:42 +0000

    Update gtag.js

--- js/gtag.js
+++ js/gtag.js
@@ -1,14 +1,3 @@
-function loadGtagScript() {
-  if (!window.gtagScriptLoaded) {
-    var script = document.createElement('script');
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-GDCEBH1ZZZ';
-    script.async = true;
-    document.head.appendChild(script);
-    window.gtagScriptLoaded = true;
-  }
-}
-loadGtagScript();
-
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
