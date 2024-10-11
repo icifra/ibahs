c3aef120ff31bd2cd41af6e29cbf58155df13b1c
Icifra <icifra@outlook.com>
2024-10-11 07:50:29 +0000

    Тестирование объявления

---
+++ javascript/announcement.js
@@ -0,0 +1,11 @@
+// Настройка скорости анимации
+// Меняет скорость анимации текста объявления
+document.addEventListener("DOMContentLoaded", function() {
+    const announcementText = document.getElementById("announcement-text");
+    
+    // Устанавливаем начальную скорость анимации (время в секундах)
+    let animationSpeed = 12; // Регулирование скорости в JS
+    
+    // Применяем скорость анимации
+    announcementText.style.animation = `scroll-text ${animationSpeed}s linear infinite`;
+});
