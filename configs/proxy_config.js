014357323558a06cce80578ce057e5935268624d
Icifra <icifra@outlook.com>
2024-11-16 06:58:05 +0000

    Рабочая версия скрипта

--- configs/proxy_config.js
+++ configs/proxy_config.js
@@ -1,12 +1,10 @@
-/* Proxy Auto-Configuration File 
- * Этот скрипт проксирует трафик для указанных доменов и их поддоменов через указанный прокси.
- */
+/* Proxy Auto-Configuration File */
 function FindProxyForURL(url, host) {
     // Сайты, которые должны использовать прокси
     if (
         dnsDomainIs(host, "chatgpt.com") || // chatgpt.com и поддомены
         dnsDomainIs(host, "openai.com") || // openai.com и поддомены
-        dnsDomainIs(host, "ipleak.net")   // сайт и поддомены
+        dnsDomainIs(host, "microsoft.com")   // сайт и поддомены
     ) {
         return "PROXY 127.0.0.1:10050"; // Указание адреса прокси
     }
