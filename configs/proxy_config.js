bc81906a5102c0e4de2ed2c54a033e188c877ed0
dos <dos@yandex.ru>
2024-12-16 16:19:33 +0000

    Корректировка прокси-листа

--- configs/proxy_config.js
+++ configs/proxy_config.js
@@ -5,9 +5,7 @@ function FindProxyForURL(url, host) {
     dnsDomainIs(host, "chatgpt.com") || // сайты и поддомены
     dnsDomainIs(host, "openai.com") ||
     dnsDomainIs(host, "claude.ai") ||
-    dnsDomainIs(host, "copilot.microsoft.com") ||
-    dnsDomainIs(host, "designer.microsoft.com") ||
-    dnsDomainIs(host, "corsair.com")
+    dnsDomainIs(host, "microsoft.com")
   ) {
     return "SOCKS 127.0.0.1:10808"; // Указание адреса прокси
   }
