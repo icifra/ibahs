6b0c356a8931fe9c53771bf5af0962077e2d368f
dos <dos@yandex.ru>
2024-12-19 07:52:37 +0000

    Исключение майкрософт

--- configs/proxy_config.js
+++ configs/proxy_config.js
@@ -4,8 +4,7 @@ function FindProxyForURL(url, host) {
   if (
     dnsDomainIs(host, "chatgpt.com") || // сайты и поддомены
     dnsDomainIs(host, "openai.com") ||
-    dnsDomainIs(host, "claude.ai") ||
-    dnsDomainIs(host, "microsoft.com")
+    dnsDomainIs(host, "claude.ai")
   ) {
     return "SOCKS 127.0.0.1:10808"; // Указание адреса прокси
   }
