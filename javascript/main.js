d01f3e65ebe35332453c20030596062f9036a577
dos <dos@yandex.ru>
2025-01-11 13:09:23 +0000

    Включение инициализации

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,12 @@
 import HLSInitializer from './hlsInitializer.js';
+import GeoDataInitializer from './geoData.js';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
+  // Инициализируем HLS
   const hlsInit = new HLSInitializer();
   hlsInit.initializeAll();
+
+  // Инициализируем гео-данные
+  const geoInit = new GeoDataInitializer();
+  await geoInit.initialize();
 });
