b5db1eeda4599527e186286657af6385219e8b55
dos <dos@yandex.ru>
2025-01-28 16:08:09 +0000

    Добавление env и настройка ConfigModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // Добавляем валидацию наличия необходимых переменных
+      validate: (config: Record<string, unknown>) => {
+        const requiredEnvs = [
+          'PORT', 
+          'NODE_ENV'
+        ];
+        for (const env of requiredEnvs) {
+          if (!config[env]) {
+            throw new Error(`Environment variable ${env} is required`);
+          }
+        }
+        return config;
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
